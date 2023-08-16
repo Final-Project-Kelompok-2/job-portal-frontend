a0ee2cd336c85e411c8267fdad9a180f39ad110b
Eril <m.erilfario19@gmail.com>
2023-08-16 17:51:44 +0700

    fix errors,bugs, and connecting dashboard data to service

--- projects/admin-jobroad/src/app/pages/benefits/list/benefit-list.component.ts
+++ projects/admin-jobroad/src/app/pages/benefits/list/benefit-list.component.ts
@@ -36,9 +36,10 @@ export class BenefitListComponent implements OnInit {
 
   insert() {
     const data = this.benefitReqDto.getRawValue();
-    this.benefitService.create(data).subscribe();
-    this.getBenefit();
-    this.benefitReqDto.reset();
-    this.visible = false;
+    this.benefitService.create(data).subscribe(result => {
+      this.getBenefit();
+      this.benefitReqDto.reset();
+      this.visible = false;
+    })
   }
 }
--- projects/admin-jobroad/src/app/pages/dashboard/dashboard.component.ts
+++ projects/admin-jobroad/src/app/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,14 @@ import { CompanyService } from "../../service/company.service";
 import { CompanyResDto } from "../../dto/company/company.res.dto";
 import { UserResDto } from "../../dto/user/user.res.dto";
 import { UserService } from "../../service/user.service";
+import { JobService } from "../../service/job.service";
+import { JobResDto } from "../../dto/job/job.res.dto";
+import { BenefitResDto } from "../../dto/benefit/benefit.res.dto";
+import { QuestionResDto } from "../../dto/question/question.res.dto";
+import { CandidateUserResDto } from "../../dto/candidate-user/candidate-user.res.dto";
+import { BenefitService } from "../../service/benefit.service";
+import { QuestionService } from "../../service/question.service";
+import { CandidateUserService } from "../../service/candidate-user.service";
 // import { AuthService } from "../../service/auth.service";
 
 @Component({
@@ -14,24 +22,37 @@ import { UserService } from "../../service/user.service";
 export class DashboardComponent implements OnInit {
 
   fullName = ''
+  salutation = ''
   companies! : CompanyResDto[]
+  jobs!: JobResDto[]
+  jobSize = 0
+  benefits!:BenefitResDto[]
+  benefitSize = 0
+  questions!:QuestionResDto[]
+  questionSize = 0
+  candidates!:CandidateUserResDto[]
+  candidateSize = 0
   companySize = 0
   users! : UserResDto[]
   userSize = 0
 
-  constructor(private authService : AuthService, private companyService:CompanyService,private userService : UserService){
+  constructor(private authService : AuthService, private companyService:CompanyService,private userService : UserService,private jobService:JobService, private benefitService:BenefitService,private questionService:QuestionService,private candidateService:CandidateUserService){
 
   }
 
   ngOnInit(): void {
-    // const profile = this.authService.getProfile()
+    const profile = this.authService.getProfile()
 
-    // if(profile){
-    //   this.fullName = profile.profileName
-    // }
+    if(profile){
+      this.fullName = profile.fullName
+      // this.salutation = profile
+    }
 
     this.getCompanies()
     this.getUsers()
+    this.getAllBenefits()
+    this.getAllJobs()
+    this.getAllQuestions()
   }
 
   getCompanies(){
@@ -45,6 +66,34 @@ export class DashboardComponent implements OnInit {
     this.userService.getAllUser().subscribe(result => {
       this.users = result
       this.userSize = this.users.length
+    })
+  }
+
+  getAllJobs(){
+    this.jobService.getAll().subscribe(result =>{
+      this.jobs = result;
+      this.jobSize = this.jobs.length
+    })
+  }
+
+  getAllBenefits(){
+    this.benefitService.getAll().subscribe(result =>{
+      this.benefits = result;
+      this.benefitSize = this.benefits.length
+    })
+  }
+
+  getAllQuestions(){
+    this.questionService.getAll().subscribe(result =>{
+      this.questions = result;
+      this.questionSize = this.questions.length
+    })
+  }
+
+  getAllCandidates(){
+    this.questionService.getAll().subscribe(result =>{
+      this.questions = result;
+      this.questionSize = this.questions.length
     })
   }
 
