2057537b2215e8b6eb416fd362f0a71201465c14
drewmaruli <201900190@my.apiu.edu>
2023-08-18 18:01:11 +0700

    Add Candidate Update Page & Details on Admin

--- projects/admin-jobroad/src/app/pages/candidates/candidate.routing.ts
+++ projects/admin-jobroad/src/app/pages/candidates/candidate.routing.ts
@@ -8,6 +8,7 @@ import { CommonModule } from "@angular/common";
 import { CandidateDetailComponent } from "./detail/candidate-detail.component";
 import { UrlPipe } from "../../pipe/url.pipe";
 import { BadgeModule } from 'primeng/badge';
+import { CandidateUpdateComponent } from "./update/candidate-update.component";
 
 const routes: Routes = [
   {
@@ -21,13 +22,18 @@ const routes: Routes = [
   {
     path:'detail/:id',
     component:CandidateDetailComponent
+  },
+  {
+    path:':id/update',
+    component:CandidateUpdateComponent
   }
 ]
 @NgModule({
   declarations: [
     CandidateListComponent,
     CandidateCreateComponent,
-    CandidateDetailComponent
+    CandidateDetailComponent,
+    CandidateUpdateComponent
   ],
   imports: [
     RouterModule.forChild(routes),
