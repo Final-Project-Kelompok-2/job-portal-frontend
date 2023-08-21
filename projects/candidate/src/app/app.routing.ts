01cac6c3d5b2fdd2900fbc342d22eff99998cf45
Eril <m.erilfario19@gmail.com>
2023-08-21 15:04:59 +0700

    created landing page

--- projects/candidate/src/app/app.routing.ts
+++ projects/candidate/src/app/app.routing.ts
@@ -10,61 +10,76 @@ import { BaseComponent } from "./component/base/base.component";
 import { BaseModule } from "./component/base/base.module";
 import { NotFoundComponent } from "./component/not-found/not-found.component";
 import { authValidation, authValidationNonlogin } from "./validation/auth.validation";
+import { LandingComponent } from "./pages/landing/landing.component";
 
 const routes: Routes = [
-    {
-        path: 'login',
-        component: LoginComponent,
-        // canMatch: [authValidation]
-    },
-    {
-        component: BaseComponent,
-        path: 'dashboard',
-        canMatch: [authValidationNonlogin],
-        children: [{
-            path: '',
-            component: DashboardComponent,
+  {
+    path: 'login',
+    component: LoginComponent,
+    canMatch: [authValidation]
+  },
+  {
+    component: BaseComponent,
+    path: 'landing',
+    canMatch: [authValidation],
+    children: [{
+      path:'',
+      component: LandingComponent,
+    }]
+  },
+  {
+    component: BaseComponent,
+    path: 'dashboard',
+    canMatch: [authValidationNonlogin],
+    children: [{
+      path: '',
+      component: DashboardComponent,
 
-        }]
-    },
-    {
-        component: BaseComponent,
-        path: 'jobs',
-        loadChildren: () => import('./pages/jobs/job.module').then(u => u.JobModule)
-    },
-    {
-        component: BaseComponent,
-        path: 'companies',
-        loadChildren: () => import('./pages/company/company.module').then(c => c.CompanyModule)
-    },
-    {
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+    }]
+  },
+  {
+    component: BaseComponent,
+    path: 'jobs',
+    canMatch: [authValidationNonlogin],
+
+    loadChildren: () => import('./pages/jobs/job.module').then(u => u.JobModule)
+  },
+  {
+    component: BaseComponent,
+    path: 'companies',
+    canMatch: [authValidationNonlogin],
+
+    loadChildren: () => import('./pages/company/company.module').then(c => c.CompanyModule)
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
 ]
 
 @NgModule({
-    declarations: [LoginComponent,
-        NotFoundComponent,
-        DashboardComponent
-    ],
-    exports: [
-        LoginComponent,
-        RouterModule,
-        BaseModule,
-    ],
-    imports: [
-        RouterModule.forRoot(routes),
-        SharedModuleComponent,
-        CommonModule,
-        ReactiveFormsModule,
-        ButtonComponent
-    ]
+  declarations: [LoginComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    LandingComponent
+  ],
+  exports: [
+    LoginComponent,
+    RouterModule,
+    BaseModule,
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    SharedModuleComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonComponent
+  ]
 }
 )
 
