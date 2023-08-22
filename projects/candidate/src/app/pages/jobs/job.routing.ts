3220a9001e1139b0d9505d145b860de2391460cd
Eril <m.erilfario19@gmail.com>
2023-08-22 10:48:46 +0700

    fix card job-list

--- projects/candidate/src/app/pages/jobs/job.routing.ts
+++ projects/candidate/src/app/pages/jobs/job.routing.ts
@@ -7,35 +7,37 @@ import { JobSavedComponent } from "./saved/job-saved.component";
 import { JobDetailComponent } from "./detail/job-detail.component";
 import { UrlPipe } from "../../pipe/url.pipe";
 import { JobAppliedComponent } from "./applied/job-applied.component";
+import { ButtonComponent } from "../../component/button/button.component";
 
 const route: Routes = [
-    {
-        path: "",
-        component: JobListComponent
-    },
-    {
-        path: "saved",
-        component: JobSavedComponent
-    },
-    {
-        path: ":id/detail",
-        component: JobDetailComponent
-    },
-    {
-        path: "applied",
-        component: JobAppliedComponent
-    }
+  {
+    path: "",
+    component: JobListComponent
+  },
+  {
+    path: "saved",
+    component: JobSavedComponent
+  },
+  {
+    path: ":id/detail",
+    component: JobDetailComponent
+  },
+  {
+    path: "applied",
+    component: JobAppliedComponent
+  }
 ]
 
 @NgModule({
-    declarations: [JobListComponent, JobSavedComponent, JobDetailComponent, JobAppliedComponent],
-    imports: [
-        RouterModule.forChild(route),
-        SharedModuleComponent,
-        CommonModule,
-        UrlPipe
-    ],
-    exports: []
+  declarations: [JobListComponent, JobSavedComponent, JobDetailComponent, JobAppliedComponent],
+  imports: [
+    RouterModule.forChild(route),
+    SharedModuleComponent,
+    CommonModule,
+    UrlPipe,
+    ButtonComponent
+  ],
+  exports: []
 })
 export class JobRouting {
 
