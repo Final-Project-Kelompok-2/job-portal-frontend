cb9aff15a424cb406101dbdb011273dd5811caa0
Eril <m.erilfario19@gmail.com>
2023-08-23 09:11:12 +0700

    edited job list and saved

--- projects/candidate/src/app/pages/jobs/saved/job-saved.component.ts
+++ projects/candidate/src/app/pages/jobs/saved/job-saved.component.ts
@@ -1,36 +1,42 @@
 import { Component, OnInit } from "@angular/core";
 import { SavedJobService } from "../../../service/saved-job.service";
 import { SavedJobResDto } from "../../../dto/saved-job/saved-job.res.dto";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'job-saved',
-    templateUrl: './job-saved.component.html'
+  selector: 'job-saved',
+  templateUrl: './job-saved.component.html',
+  styleUrls: ['./job-saved.component.css']
 })
 export class JobSavedComponent implements OnInit {
 
-    savedJobs!: SavedJobResDto[]
-    savedJobsLength = 0
+  savedJobs!: SavedJobResDto[]
+  savedJobsLength = 0
 
-    haveSaved = false
+  haveSaved = false
 
-    constructor(private savedJobService: SavedJobService) {
+  constructor(private savedJobService: SavedJobService, private router: Router) {
 
-    }
+  }
 
-    ngOnInit(): void {
-        this.getSavedJobs()
-        this.check()
-    }
+  ngOnInit(): void {
+    this.getSavedJobs()
+    this.check()
+  }
 
-    getSavedJobs() {
-        this.savedJobService.getByPrincipal().subscribe(result => {
-            this.savedJobs = result
-            this.savedJobsLength = this.savedJobs.length
-        })
-    }
+  getSavedJobs() {
+    this.savedJobService.getByPrincipal().subscribe(result => {
+      this.savedJobs = result
+      this.savedJobsLength = this.savedJobs.length
+    })
+  }
 
-    check() {
-        return this.savedJobsLength > 0
-    }
+  openDetail(id:string){
+    this.router.navigateByUrl(`jobs/${id}/detail`)
+  }
+
+  check() {
+    return this.savedJobsLength > 0
+  }
 
 }
