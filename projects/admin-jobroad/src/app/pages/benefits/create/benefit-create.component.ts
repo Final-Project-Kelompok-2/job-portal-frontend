7f4ab6016bac9df65ae1c3a0f58c63fb30eac1fd
drewmaruli <201900190@my.apiu.edu>
2023-09-01 10:10:54 +0700

    Update Question Page & Benefit

--- projects/admin-jobroad/src/app/pages/benefits/create/benefit-create.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'benefit-create',
-  templateUrl: './benefit-create.component.html'
-})
-export class BenefitCreateComponent {
-  loading = false
-}
